a96b24074b90cba2c5f6f9dd7510ca9cf77b2543
Lynn <code@krogseng.com>
2017-03-14 06:35:25 -0700

    Added mongoose connection

--- api/server.js
+++ api/server.js
@@ -2,33 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const PORT = 3000;
-const DB_URI = 'mongodb://localhost:27017/db';
-const cors = require('cors')();
+//const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/gallery';
+//const connection = require('./lib/connection');
 
-const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors')();
 
 app.use(morgan('combined'));
 app.use(cors);
 
-const connection = {
-    db: null,
-    connect(dbUri) {
-        if (this.db) return Promise.reject('Already connected');
-        return MongoClient.connect(dbUri)
-            .then(db => this.db = db);
-    },
-    close() {
-        if (!this.db) return Promise.resolve();
-        return this.db.close()
-            .then(result => {
-                this.db = null;
-                return result;
-            });
-    }
-};
-
-connection.connect(DB_URI)
-    .catch(err => console.log('db connection failed', err));
 
 app.get('/', function (req, res) {
     res.send('server is listening');
